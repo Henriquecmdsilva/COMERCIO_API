aee34c4fc628bace7a413ea7f65c34c672ea975a
Henriquecmdsilva <212362600+gtsenacti@users.noreply.github.com>
2025-07-14 20:42:09 -0300

    Revert "Alterações da API"
    This reverts commit c09b977778245f0fdea44bd3a10eb959a0320c29.

--- CONECTAR/funcaoConectar.py
+++ CONECTAR/funcaoConectar.py
@@ -1,5 +1,4 @@
 import sqlite3
 
 def conectar():
-    caminho_do_banco = r"C:\Users\994155\Documents\GitHub\APP_SQLITE\SQLiteDatabaseBrowserPortable\appComercio.db"
-    return sqlite3.connect("caminho_do_banco")
+    return sqlite3.connect("appComercio.db")  # banco no mesmo diretório
